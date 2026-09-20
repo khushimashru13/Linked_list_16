ce3c988b71e7fa733fd4fe4ebcb3af824825a857
khushimashru13 <khushimashru13@gmail.com>
2026-09-20 17:17:23 +0530

    Time: 7 ms (88.77%) | Memory: 49.8 MB (36.95%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,20 @@
+class MyHashSet {
+    vector<bool> present;
+
+public:
+    MyHashSet() {
+        present.resize(1000001, false);
+    }
+
+    void add(int key) {
+        present[key] = true;
+    }
+
+    void remove(int key) {
+        present[key] = false;
+    }
+
+    bool contains(int key) {
+        return present[key];
+    }
+};
